74ec8765b4d45b9970f257857873060db21e17f3
Oshang Surtani <106865632+Oshangsurtani@users.noreply.github.com>
2024-03-04 00:30:12 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -10,13 +10,13 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
-
+ 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4)  //negative not accept
 console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
@@ -31,5 +31,5 @@ const url = "https://hitesh.com/hitesh%20choudhary"
 console.log(url.replace('%20', '-'))
 
 console.log(url.includes('sundar'))
-
-console.log(gameName.split('-'));
+//hai ya nhi sundar url meijj
+console.log(gameName.split('-')); // array hitesh hc com
